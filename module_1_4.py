b19e83e01b3f89efe0764b4111c571ebcf91fa7d
alve071211 <alve071211@gmail.com>
2024-09-11 10:28:32 +1200

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string='Найдите себе дело по душе, и вам не придётся работать ни единого дня в своей жизни'
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+my_string=(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
