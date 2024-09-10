73235d576948f9bb810da304db4bc40936dfe048
alve071211 <alve071211@gmail.com>
2024-09-11 10:40:40 +1200

    Delete module_1_4.py

--- module_1_4.py
+++
@@ -1,7 +0,0 @@
-my_string='Найдите себе дело по душе, и вам не придётся работать ни единого дня в своей жизни'
-print(len(my_string))
-print(my_string.upper())
-print(my_string.lower())
-my_string=(my_string.replace(' ',''))
-print(my_string[0])
-print(my_string[-1])
